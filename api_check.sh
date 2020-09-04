1347e68889ffd1ad4accba70ff7b808e352d5585
ian <ian.miell@gmail.com>
2020-09-04 18:05:07 +0100

    Added a check that kubectl deprecations is available. Made output more readable. Added progress information.

--- api_check.sh
+++ api_check.sh
@@ -1,7 +1,35 @@
+#!/usr/bin/env bash
+
+if ! kubectl deprecations >/dev/null 2>&1
+then
+  echo '`kubectl deprecations` did not run ok. You may need to install the krew 'deprecations' package. See https://krew.sigs.k8s.io/'
+  exit 1
+fi
+
 if [[ $# -eq 0 ]]
 then
-    echo "No arguments supplied, reading from file"
-    while IFS= read -r line; do find . -type f -name "*.yaml" -exec echo {} \; -exec kubectl deprecations --error-on-deleted --error-on-deprecated --k8s-version=$line --input-file {} \;; done < .suported-k8s-versions
+  while IFS= read -r line
+  do
+    echo "================================================================================"
+    echo "Looking at k8s version: ${line}"
+    echo "================================================================================"
+    echo
+    for f in $(find . -type f -name "*.yaml")
+    do
+      echo -n "${f} "
+      if ! RES=$(kubectl deprecations --error-on-deleted --error-on-deprecated --k8s-version="$line" --input-file "${f}" 2>&1)
+      then
+        echo
+        echo "================================================================================"
+        echo "Problem with file: ${f} on k8s version ${line}"
+        echo "================================================================================"
+        echo "Output was: ${RES}"
+        echo "================================================================================"
+        echo
+        echo "Checking files... "
+      fi
+    done
+  done < .supported-k8s-versions
 else
-    find . -type f -name "*.yaml" -exec echo {} \; -exec kubectl deprecations --error-on-deleted --error-on-deprecated --k8s-version=v$1 --input-file {} \;
+  find . -type f -name "*.yaml" -exec echo {} \; -exec kubectl deprecations --error-on-deleted --error-on-deprecated --k8s-version="v$1" --input-file {} \;
 fi
