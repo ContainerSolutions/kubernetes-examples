e1e480f3c2c5573de0e4ec176653431eb20512c8
melihc1 <m_cinalioglu@yahoo.com.tr>
2023-05-10 19:20:54 +0300

    ignore bin for markdown check

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 cd examples
 
-if ! find . -maxdepth 1 -type d  | grep -v '^\.$' | egrep -vw '(_posts|_includes|\.gif|_sass|_layouts|_site|_sass|assets|\.github|\.git)' | xargs -IXXX ls XXX/XXX.md >/dev/null
+if ! find . -maxdepth 1 -type d  | grep -v '^\.$' | egrep -vw '(_posts|_includes|\.gif|_sass|_layouts|_site|_sass|assets|\.github|\.git|bin)' | xargs -IXXX ls XXX/XXX.md >/dev/null
 then
   echo "Add examples/FOLDER/FOLDER.md to the folder that's mentioned above"
   exit 1
