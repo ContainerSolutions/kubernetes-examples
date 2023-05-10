d436ddc8b117d1b6cf2b35410ff103b4446229c8
melihc1 <m_cinalioglu@yahoo.com.tr>
2023-05-10 19:17:25 +0300

    add release.sh to check example markdowns

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd examples
+
+if ! find . -maxdepth 1 -type d  | grep -v '^\.$' | egrep -vw '(_posts|_includes|\.gif|_sass|_layouts|_site|_sass|assets|\.github|\.git)' | xargs -IXXX ls XXX/XXX.md >/dev/null
+then
+  echo "Add examples/FOLDER/FOLDER.md to the folder that's mentioned above"
+  exit 1
+fi
+
